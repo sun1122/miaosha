d9ca72d32a191996cb641aab78935423e5606cc5
周家申 <282581063@qq.com>
2019-03-18 22:10:23 +0800

    spring boot 打war包

--- src/main/java/com/flysun/miaosha/MainApplication.java
+++ src/main/java/com/flysun/miaosha/MainApplication.java
@@ -3,21 +3,26 @@ package com.flysun.miaosha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-/**  
- * @ClassName: MainApplication  
- * @Description: TODO(这里用一句话描述这个类的作用)  
- * @author 周家申  
- * @date 2019年1月25日  
- *    
+/**
+ * @ClassName: MainApplication
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author 周家申
+ * @date 2019年1月25日
+ * 
  */
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
 
 }
-
-
